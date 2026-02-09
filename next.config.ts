6be4db95d7055f8b07fe44d8d772230cb461e790
brahimbouargane <brahim.bouargane2000@gmail.com>
2026-02-09 12:07:32 +0100

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
-  reactCompiler: true,
+  // Optional: ensures links like /about work without needing .html in the URL
+  trailingSlash: true,
+  images: {
+    // Required because the default optimization API needs a Node.js server
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
